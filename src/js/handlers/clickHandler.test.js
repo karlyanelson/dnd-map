5c7a4f3c02dcc8a4228fce1056d32d5671e1d72c
Karly Nelson <karlyanelson@gmail.com>
2021-01-12 22:47:14 -0600

    finish adding jest-dom

--- src/js/handlers/clickHandler.test.js
+++ src/js/handlers/clickHandler.test.js
@@ -4,6 +4,7 @@ import {
   getByText,
   within,
   getQueriesForElement,
+  screen,
 } from "@testing-library/dom";
 import "@testing-library/jest-dom/extend-expect";
 import userEvent from "@testing-library/user-event";
@@ -13,31 +14,42 @@ import path from "path";
 import clickHandler from "./clickHandler";
 import addCharacter from "../utils/addCharacter";
 
-// https://dev.to/thawkin3/how-to-unit-test-html-and-vanilla-javascript-without-a-ui-framework-4io
-describe("clickHandler", () => {
-  const html = fs.readFileSync(
-    path.resolve(__dirname, "../../index.html"),
-    "utf8"
-  );
+// https://testing-library.com/docs/ecosystem-jest-dom
+test("uses jest-dom", () => {
+  document.body.innerHTML = `
+    <span data-testid="not-empty"><span data-testid="empty"></span></span>
+    <div data-testid="visible">Visible Example</div>
+  `;
 
-  let dom;
-  let body;
-
-  beforeEach(() => {
-    dom = new JSDOM(html, {
-      runScripts: "dangerously",
-      resources: "usable",
-    });
-    // https://github.com/jsdom/jsdom#loading-subresources
-    body = dom.window.document.body;
-  });
-
-  it("click add character", () => {
-    const addCharacterButton = getByText(body, "Add Character");
-    userEvent.click(addCharacterButton);
-    // expect(getByText(container, "Character 1")).toBeInTheDocument();
-  });
+  expect(screen.queryByTestId("not-empty")).not.toBeEmptyDOMElement();
+  expect(screen.getByText("Visible Example")).toBeVisible();
 });
+
+// https://dev.to/thawkin3/how-to-unit-test-html-and-vanilla-javascript-without-a-ui-framework-4io
+// describe("clickHandler", () => {
+//   const html = fs.readFileSync(
+//     path.resolve(__dirname, "../../index.html"),
+//     "utf8"
+//   );
+
+//   let dom;
+//   let body;
+
+//   beforeEach(() => {
+//     dom = new JSDOM(html, {
+//       runScripts: "dangerously",
+//       resources: "usable",
+//     });
+//     // https://github.com/jsdom/jsdom#loading-subresources
+//     body = dom.window.document.body;
+//   });
+
+//   it("click add character", () => {
+//     const addCharacterButton = getByText(body, "Add Character");
+//     userEvent.click(addCharacterButton);
+//     // expect(getByText(container, "Character 1")).toBeInTheDocument();
+//   });
+// });
 
 // https://github.com/kentcdodds/dom-testing-library-with-anything/blob/master/vanilla.test.js
 // test("add character", () => {
