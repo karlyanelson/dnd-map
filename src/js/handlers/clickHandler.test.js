97b523a1b4099eb47a58f0a061214fac03ced253
Karly Nelson <karlyanelson@gmail.com>
2021-01-12 23:29:07 -0600

    try inlining css and js in dist

--- src/js/handlers/clickHandler.test.js
+++ src/js/handlers/clickHandler.test.js
@@ -15,41 +15,46 @@ import clickHandler from "./clickHandler";
 import addCharacter from "../utils/addCharacter";
 
 // https://testing-library.com/docs/ecosystem-jest-dom
-test("uses jest-dom", () => {
-  document.body.innerHTML = `
-    <span data-testid="not-empty"><span data-testid="empty"></span></span>
-    <div data-testid="visible">Visible Example</div>
-  `;
+// test("uses jest-dom", () => {
+//   document.body.innerHTML = `
+//     <span data-testid="not-empty"><span data-testid="empty"></span></span>
+//     <div data-testid="visible">Visible Example</div>
+//   `;
 
-  expect(screen.queryByTestId("not-empty")).not.toBeEmptyDOMElement();
-  expect(screen.getByText("Visible Example")).toBeVisible();
-});
+//   expect(screen.queryByTestId("not-empty")).not.toBeEmptyDOMElement();
+//   expect(screen.getByText("Visible Example")).toBeVisible();
+// });
 
 // https://dev.to/thawkin3/how-to-unit-test-html-and-vanilla-javascript-without-a-ui-framework-4io
-// describe("clickHandler", () => {
-//   const html = fs.readFileSync(
-//     path.resolve(__dirname, "../../index.html"),
-//     "utf8"
-//   );
+// https://github.com/thawkin3/dom-testing-demo/tree/master/src
+describe("clickHandler", () => {
+  const html = fs.readFileSync(
+    path.resolve(__dirname, "../../../dist/index.html"),
+    "utf8"
+  );
 
-//   let dom;
-//   let body;
+  let dom;
+  let body;
 
-//   beforeEach(() => {
-//     dom = new JSDOM(html, {
-//       runScripts: "dangerously",
-//       resources: "usable",
-//     });
-//     // https://github.com/jsdom/jsdom#loading-subresources
-//     body = dom.window.document.body;
-//   });
+  beforeEach(() => {
+    dom = new JSDOM(html, {
+      url: "http://localhost:1234/",
+      runScripts: "dangerously",
+      resources: "usable",
+      beforeParse(window) {
+        window.requestAnimationFrame = () => {};
+      },
+    });
+    // https://github.com/jsdom/jsdom#loading-subresources
+    body = dom.window.document.body;
+  });
 
-//   it("click add character", () => {
-//     const addCharacterButton = getByText(body, "Add Character");
-//     userEvent.click(addCharacterButton);
-//     // expect(getByText(container, "Character 1")).toBeInTheDocument();
-//   });
-// });
+  it("click add character", () => {
+    const addCharacterButton = getByText(body, "Add Character");
+    fireEvent.click(addCharacterButton);
+    expect(getByText(body, "Character 1")).toBeInTheDocument();
+  });
+});
 
 // https://github.com/kentcdodds/dom-testing-library-with-anything/blob/master/vanilla.test.js
 // test("add character", () => {
