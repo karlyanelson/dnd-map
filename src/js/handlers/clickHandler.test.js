8ce8c879ed04c6db1343653c95f77d682fde6b26
Karly Nelson <karlyanelson@gmail.com>
2021-01-14 17:25:11 -0600

    tried lots of different options

--- src/js/handlers/clickHandler.test.js
+++ src/js/handlers/clickHandler.test.js
@@ -20,6 +20,136 @@ import { get } from "https";
 
 import CharacterList from "../components/CharacterList";
 
+test("dist/index.html - build - scripts inline - no Reef :(", async () => {
+  // only works with bundled js
+  // bundled js needs to be inline in a script tag in the index.html
+  // works with doSomething() function, but not Reef things
+
+  const html = fs.readFileSync(
+    path.resolve(__dirname, "../../../dist/index.html"),
+    "utf8"
+  );
+
+  const dom = new JSDOM(html, {
+    url: "http://localhost:1234/",
+    runScripts: "dangerously",
+    beforeParse(window) {
+      window.requestAnimationFrame = () => {};
+    },
+  });
+
+  // const container = dom.window.document.body;
+  const container = dom.window.document.querySelector("#sectionCharacters");
+
+  const { getByText, findByText } = getQueriesForElement(container);
+  const addCharacterButton = getByText("Add Character");
+  userEvent.click(addCharacterButton);
+  expect(await findByText("I did it!")).toBeInTheDocument();
+});
+
+xtest("JSDOM.fromFile('./dist/index.html' - build - inline? - Reef?", async () => {
+  // only works with bundled js
+  // ideally would work with scripts not inline but doesn't quite yet
+
+  const options = {
+    runScripts: "dangerously",
+    resources: "usable",
+  };
+
+  const dom = await JSDOM.fromFile("./dist/index.html", options);
+
+  const container = dom.window.document.querySelector("#sectionCharacters");
+
+  const { getByText, findByText } = getQueriesForElement(container);
+  const addCharacterButton = getByText("Add Character");
+  userEvent.click(addCharacterButton);
+  expect(await findByText("I did it!")).toBeInTheDocument();
+});
+
+xtest("http://localhost:1234/index.html - localhost - scripts NOT inline - Reef?", async () => {
+  // will load external script tag
+  // but need to run localhost at the same time as tests
+
+  const html = fs.readFileSync("http://localhost:1234/index.html");
+
+  const dom = new JSDOM(html, {
+    url: "http://localhost:1234/",
+    runScripts: "dangerously",
+    resources: "usable",
+    beforeParse(window) {
+      window.requestAnimationFrame = () => {};
+    },
+  });
+
+  const container = dom.window.document.querySelector("#sectionCharacters");
+
+  const { getByText, findByText } = getQueriesForElement(container);
+  const addCharacterButton = getByText("Add Character");
+  userEvent.click(addCharacterButton);
+  expect(await findByText("I did it!")).toBeInTheDocument();
+});
+
+// test("Add Character button adds character", () => {
+//   // const container = document.querySelector("#mainControlsContent");
+
+//   const addCharacterButton = screen.getByText("Add Character");
+
+//   const event = userEvent.click(addCharacterButton);
+
+//   // const myEvent = createEvent.click(addCharacterButton);
+
+//   console.log(event);
+
+//   clickHandler(event);
+
+//   // expect(screen.getByText("Character 1")).toBeVisible();
+// });
+
+// test("Add Character button adds character", () => {
+//   document.body.innerHTML = `
+//     <button class="button" id="addCharacter">Add Character</button>
+//     <div id="characterList"></div>
+//   `;
+
+//   const addCharacterButton = screen.getByRole("button", {
+//     name: /Add Character/i,
+//   });
+//   const event = userEvent.click(addCharacterButton);
+
+//   // const myEvent = createEvent.click(addCharacterButton);
+
+//   // console.log("event", event);
+
+//   // clickHandler(event);
+
+//   // expect(screen.getByText("Character 1")).toBeVisible();
+// });
+
+// test("Add Character button adds character", () => {
+//   document.body.innerHTML = `
+//     <button class="button" id="addCharacter">Add Character</button>
+//     <div id="characterList"></div>
+//   `;
+
+//   const addCharacterButton = screen.getByText("Add Character");
+//   userEvent.click(addCharacterButton);
+
+//   expect(screen.getByText("Character 1")).toBeVisible();
+// });
+
+// https://testing-library.com/docs/ecosystem-jest-dom
+// test("uses jest-dom", () => {
+//   document.body.innerHTML = `
+//     <span data-testid="not-empty"><span data-testid="empty"></span></span>
+//     <div data-testid="visible">Visible Example</div>
+//   `;
+
+//   expect(screen.queryByTestId("not-empty")).not.toBeEmptyDOMElement();
+//   expect(screen.getByText("Visible Example")).toBeVisible();
+// });
+
+//////////
+
 // test("Add Character button adds character", () => {
 //   // const container = document.querySelector("#mainControlsContent");
 
@@ -94,29 +224,71 @@ import CharacterList from "../components/CharacterList";
 
 // https://dev.to/thawkin3/how-to-unit-test-html-and-vanilla-javascript-without-a-ui-framework-4io
 // https://github.com/thawkin3/dom-testing-demo/tree/master/src
-test("test index.html click add character", async () => {
-  const html = fs.readFileSync(
-    path.resolve(__dirname, "../../../dist/index.html"),
-    "utf8"
-  );
 
-  const dom = new JSDOM(html, {
-    url: "http://localhost:1234/",
-    runScripts: "dangerously",
-    // https://github.com/jsdom/jsdom#loading-subresources
-    // resources: "usable",
-    beforeParse(window) {
-      window.requestAnimationFrame = () => {};
-    },
-  });
-  const container = dom.window.document.body;
-  // const container = dom.window.document.querySelector("#sectionCharacters");
+// test("test index.html click add character", async () => {
+//   // const html = fs.readFileSync(
+//   //   // "http://localhost:1234/index.html",
+//   //   // path.resolve(__dirname, "../../../dist/index.html"),
+//   //   path.resolve(__dirname, "../../index.html"),
+//   //   "utf8"
+//   // );
 
-  const { getByText, findByText } = getQueriesForElement(container);
-  const addCharacterButton = getByText("Add Character");
-  userEvent.click(addCharacterButton);
-  expect(await findByText("I did it!")).toBeInTheDocument();
-});
+//   // https://stackoverflow.com/questions/53236280/how-to-load-local-javascript-file-with-jsdom
+//   // JSDOM.env(
+//   //   {
+//   //     html: html,
+//   //     // documentRoot: __dirname + "/js",
+//   //     documentRoot: __dirname,
+//   //     scripts: ["index.js"],
+//   //   },
+//   //   function (err, window) {
+//   //     console.log(window.loadFromJSDOM);
+//   //   }
+//   // );
+
+//   // const dom = new JSDOM(html, {
+//   //   // url: "http://localhost:1234/",
+//   //   runScripts: "dangerously",
+//   //   // https://github.com/jsdom/jsdom#loading-subresources
+//   //   resources: "usable",
+//   //   beforeParse(window) {
+//   //     window.requestAnimationFrame = () => {};
+//   //   },
+//   // });
+
+//   const options = {
+//     runScripts: "dangerously",
+//     resources: "usable",
+//   };
+
+//   // let dom;
+//   // JSDOM.fromFile("../../index.html", options).then((content) => {
+//   //   // console.log(content.serialize());
+
+//   //   dom = content;
+//   // });
+
+//   // const dom = await JSDOM.fromFile("../../index.html", options);
+//   const dom = await JSDOM.fromFile("./dist/index.html", options);
+
+//   ////// Maybe how to get screen to work?
+//   /////// https://stackoverflow.com/questions/45068932/jsdom-env-not-working-on-node-js-c9
+//   // var jsdom = require('jsdom');
+//   // const { JSDOM } = jsdom;
+//   // const { window } = new JSDOM();
+//   // const { document } = (new JSDOM('')).window;
+//   // global.document = document;
+
+//   // var $ = jQuery = require('jquery')(window);
+//   //////
+//   // const container = dom.window.document.body;
+//   const container = dom.window.document.querySelector("#sectionCharacters");
+
+//   const { getByText, findByText } = getQueriesForElement(container);
+//   const addCharacterButton = getByText("Add Character");
+//   userEvent.click(addCharacterButton);
+//   expect(await findByText("I did it!")).toBeInTheDocument();
+// });
 
 // https://github.com/kentcdodds/dom-testing-library-with-anything/blob/master/vanilla.test.js
 // test("add character", () => {
