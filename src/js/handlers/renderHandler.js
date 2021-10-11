6ca3fed3e8b8e53a187055123da63ad4a9b2a6b2
Nelson, Karly <karly.nelson@hey.com>
2021-10-11 13:04:29 -0500

    add ability to manage the settings and the map in different windows

--- src/js/handlers/renderHandler.js
+++ src/js/handlers/renderHandler.js
@@ -2,14 +2,26 @@ import * as _ from "../globals/variables";
 import DATA_STORE from "../globals/store";
 
 export default function renderHandler() {
-  _.pieceSizeInput.value = DATA_STORE.data.pieceSize;
-  _.mapZoomInput.value = DATA_STORE.data.zoom;
-  _.characterCount.textContent = DATA_STORE.data.characters.length;
-  _.mapSrcLabel.textContent = DATA_STORE.data.map ? "Change Map" : "Upload Map";
+  if (_.pieceSizeInput) {
+    _.pieceSizeInput.value = DATA_STORE.data.pieceSize;
+  }
+  if (_.mapZoomInput) {
+    _.mapZoomInput.value = DATA_STORE.data.zoom;
+  }
+  if (_.characterCount) {
+    _.characterCount.textContent = DATA_STORE.data.characters.length;
+  }
+  if (_.mapSrcLabel) {
+    _.mapSrcLabel.textContent = DATA_STORE.data.map
+      ? "Change Map"
+      : "Upload Map";
+  }
 
-  DATA_STORE.data.settingsExpanded
-    ? _.mainControlsContent.classList.remove("collapsed")
-    : _.mainControlsContent.classList.add("collapsed");
+  if (_.mainControlsContent) {
+    DATA_STORE.data.settingsExpanded
+      ? _.mainControlsContent.classList.remove("collapsed")
+      : _.mainControlsContent.classList.add("collapsed");
+  }
 
   DATA_STORE.data.map
     ? document.body.classList.remove("no-map")
