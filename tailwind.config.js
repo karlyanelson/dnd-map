9a3c0f14a15b720ca5bb6bc119bb48d6fe711224
Nelson, Karly <karly.nelson@hey.com>
2021-10-12 14:58:21 -0500

    add drop shadow for buttons on hover

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
       },
       boxShadow: {
         button: "2px 2px 0 black",
+        "button-hover": "2px 2px 0 black, 2px 2px 6px 0 black",
         piece: "1px 1px 1px 2px black",
         high: "2px 4px 16px 4px rgba(0, 0, 0, .25), 2px 4px 8px 4px rgba(0, 0, 0, 0.25)",
       },
